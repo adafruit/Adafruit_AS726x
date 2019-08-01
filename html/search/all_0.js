b7cbafeb352be481415bd2590a28baf8a5d7ac62
Travis CI <travis@travis-ci.org>
2019-08-01 17:29:44 +0000

    Deploy code docs to GitHub Pages Travis build: 12
    Commit: 2862be25abf5e53fc84db82fdae3cbc4034036bd

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -6,6 +6,5 @@ var searchData=
   ['as726x_5faddress',['AS726x_ADDRESS',['../_adafruit___a_s726x_8h.html#a01ac0402b36f3f8aca3ae83606304cad',1,'Adafruit_AS726x.h']]],
   ['as726x_5fintegration_5ftime_5fmult',['AS726x_INTEGRATION_TIME_MULT',['../_adafruit___a_s726x_8h.html#a4db9ce6b037d3b1db32703c7625f5589',1,'Adafruit_AS726x.h']]],
   ['as726x_5fnum_5fchannels',['AS726x_NUM_CHANNELS',['../_adafruit___a_s726x_8h.html#aa018fa7207fcdd3bb87c727566392dd7',1,'Adafruit_AS726x.h']]],
-  ['adafruit_20as726x_20spectral_20sensor',['Adafruit AS726x spectral sensor',['../index.html',1,'']]],
-  ['adafruit_20as726x_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fas726x_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fas726x_29',['Adafruit AS726x Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_AS726x.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_AS726x)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20as726x_20spectral_20sensor',['Adafruit AS726x spectral sensor',['../index.html',1,'']]]
 ];
